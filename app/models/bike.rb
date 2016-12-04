30168e9543d99c4dfad1a9664c8bcd8b11d750f1
Hesham Salah <heshamsalahuldeen@gmail.com>
2016-12-04 23:26:10 +0200

    some mods in search

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,14 +1,21 @@
 class Bike < ActiveRecord::Base
 
   def self.search(search)
-    if search[:name] || search[:style]
-      where(["    name LIKE ?
-              AND style LIKE ?
-              AND price BETWEEN ? AND ?",
-              "%#{search[:name]}%",
-              "%#{search[:style]}%",
-              "#{search[:min_price]}",
-              "#{search[:max_price]}"]).
+    k = "name LIKE ? AND style LIKE ?"
+    query = []
+    query.push(k)
+    query.push("%#{search[:name]}%")
+    query.push("%#{search[:style]}%")
+    if search[:name] || search[:style] || search[:max_price] || search[:min_price]
+      if search[:min_price] != ''
+        query[0] = query[0] + " AND price > ?"
+        query.push("#{search[:min_price]}")
+      end
+      if search[:max_price] != ''
+        query[0] = query[0] + "AND price < ?"
+        query.push("#{search[:max_price]}")
+      end
+      where(query).
       order("#{search[:order]}")
     else
       all
